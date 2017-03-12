fe0a85b0ef78404d7d38e8e4e421a4f9d7ecce2d
susuaung <susuaung.ms@gmail.com>
2017-03-12 23:13:28 +0900

    Conditional rendering

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: "Javascript: The Good Parts"},
-    { title: "Harry Potter"},
-    { title: "Girl on the Train"},
-    { title: "Blink"}
+    { title: "Javascript: The Good Parts", pages: 101},
+    { title: "Harry Potter", pages: 39},
+    { title: "Girl on the Train", pages: 85},
+    { title: "Blink", pages: 1}
   ];
 }
